6f274fa1c6088747bcb957e0de4fab450e23015f
anroche <anroche@student.42.fr>
2022-07-02 15:04:38 +0200

    setup project

---
+++ src/Components/Organisms/Meteo/index.svelte
@@ -0,0 +1,15 @@
+<div >
+	<h1>METEO</h1>
+</div>
+
+<script lang="ts">
+
+	if (typeof window !== "undefined") {
+		let meteo = fetch("http://api.meteo-concept.com/api/location/city?token=c72cdeb9a0eee5146770ddb5ca0167dcccf9f0173eb24fa0aff6988b0b9bc666&insee=35238").
+		then(e => console.log('THEN =>', e))
+		console.log('METEO =>', meteo)
+	} else {
+		console.log('NO WINDOW =>')
+	}
+
+</script>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div >
+	<h1>Tableau d'information</h1>
+	<Card padded style="display:flex;justify-content: center;  background-color: red; bottom: 16px; left: 16px;">
+<!--		<Meteo/>-->
+	</Card>
+	<p>Made by <a href="https://www.e2r5.tech">E2R5</a></p>
+</div>
+
+<script lang="ts">
+	import Card from '@smui/card';
+	import Meteo from '../Components/Organisms/Meteo/index.svelte';
+</script>
